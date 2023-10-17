d6bdb7170520a3853902193926fd38b988d109e1
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-16 22:10:34 -0300

    primeiros passos

---
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.github.sandros22.springboot.primeirowebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello!";
+	}
+
+}
