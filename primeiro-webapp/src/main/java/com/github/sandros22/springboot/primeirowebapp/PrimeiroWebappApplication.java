b5e3f9450442bf34130cf148f4e7ddc59f7be9f4
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-16 21:53:54 -0300

    initial commit

---
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/PrimeiroWebappApplication.java
@@ -0,0 +1,13 @@
+package com.github.sandros22.springboot.primeirowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroWebappApplication.class, args);
+	}
+
+}
---
+++ primeiro-webapp/src/test/java/com/github/sandros22/springboot/primeirowebapp/PrimeiroWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sandros22.springboot.primeirowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
