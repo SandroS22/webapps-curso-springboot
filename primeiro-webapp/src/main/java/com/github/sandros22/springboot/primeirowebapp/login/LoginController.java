b803163956a45cf605431de429e74fff00511aed
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-16 22:51:42 -0300

    login page

---
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.github.sandros22.springboot.primeirowebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("login")
+	public String goToLoginPage() {
+		return "login";
+	}
+}
--- primeiro-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+++ primeiro-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -3,6 +3,8 @@
 		<title>First HTML Page - JSP</title>
 	</head>
 	<body>
+		<h1>Heading 1</h1>
+		<h2>Heading 2</h2>
 		First HTML page with body - JSP
 	</body>
 </html>
