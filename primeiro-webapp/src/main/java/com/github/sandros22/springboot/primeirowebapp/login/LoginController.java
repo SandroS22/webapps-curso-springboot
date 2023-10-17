8b991054b72844ee9245decdcfd1e54cac9534d6
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-16 23:12:29 -0300

    removendo logger

--- primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/login/LoginController.java
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/login/LoginController.java
@@ -1,7 +1,5 @@
 package com.github.sandros22.springboot.primeirowebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-	private Logger logger = LoggerFactory.getLogger(getClass());
-
 	@RequestMapping("login")
 	public String goToLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
-		logger.debug("Request param is {}", name);
-		logger.info("Printando em info level");
 		return "login";
 	}
 }
