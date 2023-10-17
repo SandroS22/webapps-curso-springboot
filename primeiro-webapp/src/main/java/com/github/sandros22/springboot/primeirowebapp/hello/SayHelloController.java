dcf13ef2e7a50f9246912995a106fe6bd3cd1858
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-16 22:39:15 -0300

    html e jsp

--- primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/hello/SayHelloController.java
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/hello/SayHelloController.java
@@ -13,4 +13,25 @@ public class SayHelloController {
 		return "Hello!";
 	}
 
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> First HTML Page</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("First HTML page with body");
+		sb.append("</body>");
+		sb.append("</html>");
+
+		return sb.toString();
+	}
+
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
+
 }
---
+++ primeiro-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>First HTML Page - JSP</title>
+	</head>
+	<body>
+		First HTML page with body - JSP
+	</body>
+</html>
