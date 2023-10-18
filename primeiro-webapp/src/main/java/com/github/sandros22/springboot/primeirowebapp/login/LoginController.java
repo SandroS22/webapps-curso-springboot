08b4f70c8e1e339f955893c2b0b8a2b9337b7456
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-17 22:54:01 -0300

    form login

--- primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/login/LoginController.java
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/login/LoginController.java
@@ -1,16 +1,14 @@
 package com.github.sandros22.springboot.primeirowebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping("login")
-	public String goToLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
+	public String goToLoginPage() {
+
 		return "login";
 	}
 }
