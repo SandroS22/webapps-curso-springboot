5f2e49587b7d44f9eb877f107fa2d759ac64d685
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-10-17 23:40:25 -0300

    list todos v1

--- primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/todo/TodoService.java
+++ primeiro-webapp/src/main/java/com/github/sandros22/springboot/primeirowebapp/todo/TodoService.java
@@ -1,11 +1,15 @@
 package com.github.sandros22.springboot.primeirowebapp.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
 
-	private static List<Todo> todos;
+	private static List<Todo> todos = new ArrayList<>();
 	static {
 		todos.add(new Todo(1, "Sandro", "Aprender Angular", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(2, "Sandro", "Aprender Docker", LocalDate.now().plusYears(1), false));
---
+++ primeiro-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>List Todos PageP</title>
+	</head>
+	<body>
+		<div>Your Todos are ${todos}</div>
+	</body>
+</html>
